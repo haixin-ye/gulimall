a2335f339821d849fdf07b11ed84990c3e657dfb
haixin-ye <yhx734202@gmail.com>
2025-11-06 16:20:12 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/com/yhx/gulimall/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhx.gulimall.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
